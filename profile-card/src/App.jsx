e892f49d88ec0ac4f9bdce703ae3453cfcd0bbfa
davidrapu <168489960+davidrapu@users.noreply.github.com>
2025-07-28 12:23:35 +0100

    Profile card made

--- profile-card/src/App.jsx
+++ profile-card/src/App.jsx
@@ -1,35 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import profileImg from './assets/profile-image.jpg'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const langs = ['HTML + CSS', 'JavaScript', 'Web Design', 'Git and GitHub', 'React', ]
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="box">
+      <Avatar />
+      <Description />
+      <div className="progLangContainer">
+        {langs.map((value, index) => (
+          <Language key={index} codeLang={value} />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
+}
+function Avatar() {
+  return <img src={profileImg} alt="profile Image" />;
+}
+function Description() {
+  return <div className='descBox'>
+    <h3>David Rapu</h3>
+    <span>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus quis labore aut excepturi, repellendus omnis sunt delectus velit cupiditate, enim libero in! Dicta saepe beatae officiis. Maiores ex ullam praesentium!
+    </span>
+  </div>
+}
+
+function Language(props) {
+  const levels = ["💀", "😎", "🤓"];
+  const col = "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+  return (
+    <div style={{ backgroundColor: col  }} className="progLang">
+      <span>{props.codeLang}</span>
+      <span style={{ marginLeft: "1em" }}>
+        {levels[Math.floor(Math.random() * levels.length)]}
+      </span>
+    </div>
+  );
 }
 
 export default App
