bba53a52c23ff24b13b4f2251ef84740814e0b6c
davidrapu <168489960+davidrapu@users.noreply.github.com>
2025-07-31 23:46:39 +0100

    Made files for all components following best practices

--- travel-list/src/App.jsx
+++ travel-list/src/App.jsx
@@ -1,25 +1,17 @@
 import { useState } from "react";
 import AddingForm from "./components/AddingForm";
 import Display from "./components/Display";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   const [items, setItems] = useState([]);
-  const packed = items.filter((value) => value.packed === true);
-  const percentagePacked =
-    items.length != 0 ? Math.floor((packed.length / items.length) * 100) : 0;
   return (
     <div className="app">
-      <header>
-        <h1>🏝️FAR AWAY💼</h1>
-      </header>
+      <Header />
       <AddingForm items={items} setItems={setItems} />
       <Display items={items} setItems={setItems} />
-      <div className="stats">
-        <h3>
-          💼 You have {items.length} items on your list, and you already packed{" "}
-          {packed.length} ({percentagePacked}%)
-        </h3>
-      </div>
+      <Footer items={items} />
     </div>
   );
 }
